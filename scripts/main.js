83321ed6380cfdeb245c1263521972cdb74ecf40
Alan Moreno <alan.moreno@hotmail.com>
2024-02-21 23:50:52 -0300

    update html, css, javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
+// localStorage
+
+//Screenshot Region
+
 // flip chat-header-edition
 let msgsCounterTag = document.getElementById('screen-chat-header-edition-counter');
 let msgsCounter = parseInt(msgsCounterTag.textContent);
+
 
 function flipChatHeader(msgsCounter){
 
@@ -10,7 +15,7 @@ function flipChatHeader(msgsCounter){
 
     msgsCounterTag.textContent = msgsCounter;
 
-    if(msgsCounter === 0) {
+    if(msgsCounter === 0){
         headerDisplay.classList.add('screen-chat-headers');
         headerDisplay.classList.remove('display-none');
         headerEdition.classList.add('display-none');
@@ -23,7 +28,7 @@ function flipChatHeader(msgsCounter){
         headerEdition.classList.remove('display-none');
     }
 
-    if(msgsCounter === 1) {
+    if(msgsCounter === 1){
         buttonMsgEdit.classList.add('button-chat-edition');
         buttonMsgEdit.classList.remove('display-none');
     }else{
@@ -36,7 +41,7 @@ function flipChatHeader(msgsCounter){
 let bubblesIdCount = 0;
 
 //chat-header-edition changing side of Msgs
-function cleanSelection() {
+function cleanSelection(){
     maskedBubblesList.forEach(function (id) {
         let bubbleToClean = document.getElementById(id).getElementsByClassName('msg-selector')[0];
         if (bubbleToClean) {
@@ -48,7 +53,7 @@ function cleanSelection() {
     flipChatHeader(msgsCounter);
 }
 
-function bubbleDelete() {
+function bubbleDelete(){
     maskedBubblesList.forEach(function (id) {
         let bubbleToDelete = document.getElementById(id);
         if (bubbleToDelete) {
@@ -59,7 +64,7 @@ function bubbleDelete() {
     flipChatHeader(msgsCounter);
 }
 
-function bubbleLeft() {
+function bubbleLeft(){
     maskedBubblesList.forEach(function (id) {
         let bubbleToLeft = document.getElementById(id);
         if (bubbleToLeft) {
@@ -71,7 +76,7 @@ function bubbleLeft() {
     });
 }
 
-function bubbleCenter() {
+function bubbleCenter(){
     maskedBubblesList.forEach(function (id) {
         let bubbleToCenter = document.getElementById(id);
         if (bubbleToCenter) {
@@ -80,10 +85,15 @@ function bubbleCenter() {
             msgBubble.classList.remove('msg-bubble-left');
             msgBubble.classList.remove('msg-bubble-right');
         }
+        /*
+        let msgSelector = bubbleToCenter.getElementsByClassName('msg-selector')[0];
+        msgDeselect(msgSelector);
+        console.log(id, msgSelector);
+        */
     });
 }
 
-function bubbleRight() {
+function bubbleRight(){
     maskedBubblesList.forEach(function (id) {
         let bubbleToRight = document.getElementById(id);
         if (bubbleToRight) {
@@ -98,63 +108,71 @@ function bubbleRight() {
 // chat-main msg Selection
 let maskedBubblesList = [];
 
-function msgSelect(bubble) {
+function msgSelect(bubble){
     let maskedBubble = bubble.parentNode.parentNode.getElementsByClassName('msg-selector')[0];
     maskedBubble.classList.add('mask-selection');
     maskedBubble.classList.remove('display-none');
     msgsCounter++;  
     flipChatHeader(msgsCounter);
+    console.log(msgsCounter);
     
     maskedBubblesList.push(maskedBubble.parentNode.id);
+    console.log(maskedBubblesList);
 
 }
 
-function msgDeselect(msgSelector) {
+function msgDeselect(msgSelector){
     msgSelector.classList.add('display-none');
     msgSelector.classList.remove('mask-selection');
     msgsCounter--;
     flipChatHeader(msgsCounter);
+    console.log(msgsCounter);
 
     let indexMaskedBubblesList = maskedBubblesList.indexOf(msgSelector.parentNode.id);
-    if (indexMaskedBubblesList !== -1) {
+    if (indexMaskedBubblesList !== -1){
         maskedBubblesList.splice(indexMaskedBubblesList, 1);
     }
-
+    console.log(maskedBubblesList);
 }
 
 // chat-main msg ticks
-const iconDone = '/medias//icons/done_FILL0_wght500_GRAD0_opsz48.svg';
-const iconDoneAll = '/medias/icons/done_all_FILL0_wght500_GRAD0_opsz48.svg';
-const iconDoneAllBlue = '/medias/icons/done_all_blue_FILL0_wght500_GRAD0_opsz48.svg';
-const iconSchedule = '/medias/icons/schedule_FILL0_wght400_GRAD0_opsz24.svg'
-
-function changeTicks(button) {
-
-    let btBubbleDone = button.querySelector('img');
-    let num = (parseInt(button.value) % 4) + 1;
-
-    switch (num) {
-        case 1:
-            btBubbleDone.src = iconDoneAll;
-            break;
-        case 2:
-            btBubbleDone.src = iconDoneAllBlue;
-            break;
-        case 3:
-            btBubbleDone.src = iconSchedule;
-            break;
-        case 4:
-            btBubbleDone.src = iconDone;
-            break;
+const tickIcons = [
+    {
+        'value': '1',
+        'icon': '/medias/icons/done_all_FILL0_wght500_GRAD0_opsz48.svg'
+    },
+    {
+        'value': '2',
+        'icon': '/medias/icons/done_all_blue_FILL0_wght500_GRAD0_opsz48.svg'
+    },
+    {
+        'value': '3',
+        'icon': '/medias/icons/schedule_FILL0_wght400_GRAD0_opsz24.svg'
+    },
+    {
+        'value': '4',
+        'icon': '/medias//icons/done_FILL0_wght500_GRAD0_opsz48.svg'
     }
-    button.value = num;
+];
+
+let tickCounter = 1;
+
+
+function changeTick(button){
+    tickCounter = (parseInt(button.value) % 4) + 1;
+    let btBubbleDone = button.querySelector('img');
+    let pickIcon = tickIcons.find(item => item.value == tickCounter);
+    btBubbleDone.src = pickIcon.icon;
+    button.value = tickCounter;
 }
 
 // chat-footer
 const buttonPhoto = document.getElementById('screen-chat-footer-photo');
 const buttonMic = document.getElementById('screen-chat-footer-mic');
 const buttonSend = document.getElementById('screen-chat-footer-send');
+const panelSend = document.getElementById('screen-chat-footer-send-panel');
 const msgField = document.getElementById('screen-chat-footer-input-msg-field');
+const chatMain = document.getElementById('screen-chat-main');
 
 msgField.addEventListener('input', function () {
     if(msgField.textContent != ""){
@@ -167,3 +185,63 @@ msgField.addEventListener('input', function () {
         buttonSend.classList.add("display-none");
     }
 });
+
+//chat-send
+function sendMessage(){
+    panelSend.classList.add('chat-send-panel');
+    panelSend.classList.remove('display-none');
+}
+
+function collapseSendPanel(){
+    panelSend.classList.add('display-none');
+    panelSend.classList.remove('chat-send-panel');
+    buttonPhoto.classList.remove("display-none");
+    buttonMic.classList.remove("display-none");
+    buttonSend.classList.add("display-none");
+}
+
+function cleanMsgField(){
+    msgField.textContent = '';
+}
+
+let whatTimeIsIt = '00:00';
+function currentTime() {
+    let now = new Date();
+    let hours = now.getHours().toString().padStart(2, '0');
+    let minutes = now.getMinutes().toString().padStart(2, '0');
+    whatTimeIsIt = hours + ':' + minutes;
+}
+
+function postMsg(prop){
+    let msg = msgField.innerHTML.replace(/\<div\>/g, '<br>').replace(/\<\/div\>/g, '');
+
+    cleanMsgField();
+    collapseSendPanel();
+
+    bubblesIdCount++;
+
+    currentTime();
+
+    let pickIcon = tickIcons.find(item => item.value == tickCounter);
+
+    let msgDefault = `\<button onclick='msgDeselect(this);' class='msg-selector display-none'\>\<\/button\><div class='msg-bubble ${prop}'\>${msg}\<button onclick='msgSelect(this);' class='button-select'\>\<\/button\>\<div class='msg-bubble-metadata'\>\<p class='msg-bubble-time'\>${whatTimeIsIt}\<\/p\>\<button onclick='changeTick(this);' value='${tickCounter}' class='msg-bubble-tick'\>\<img src='${pickIcon.icon}'\/\>\<\/button\>\<\/div\>\<\/div\>`;
+
+    let newMsg = document.createElement('div');
+    newMsg.id = 'chat-msg-'+bubblesIdCount;
+    newMsg.setAttribute('class','msg-container');
+    newMsg.innerHTML = msgDefault;
+
+    chatMain.appendChild(newMsg);
+}
+
+function sendToMe(){
+    postMsg('msg-bubble-right');
+}
+
+function sendToInfo(){
+    postMsg('msg-bubble-center');
+}
+
+function sendToPeople(people){
+    postMsg('msg-bubble-left');
+}
