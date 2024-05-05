3c709955af4848ad44567676b40f9be843575a1e
Alan Moreno <alan.moreno@hotmail.com>
2024-05-04 23:43:34 -0300

    update chats

--- scripts/chats.js
+++ scripts/chats.js
@@ -1,17 +1,64 @@
 let chats = [
     {
-        chat_id: "1",
+        chat_id: "0",
         phone: "5500999990002",
         msgs: [
             {
                 msgId: "0",
                 time: "12:00",
-                side: "left",
-                idresp: "",
+                author: "me",
+                respId: "",
                 resp: "",
-                msg: "qualquer coisa",
+                msg: "first text"
             },
-
+            {
+                msgId: "1",
+                time: "12:01",
+                author: "Flávia",
+                respId: "",
+                resp: "",
+                msg: "second msg"
+            },
+            {
+                msgId: "2",
+                time: "12:01",
+                author: "me",
+                respId: "",
+                resp: "",
+                msg: "third msg"
+            },
+            {
+                msgId: "3",
+                time: "12:02",
+                author: "me",
+                respId: "",
+                resp: "",
+                msg: "lorem ipsum 4"
+            },
+            {
+                msgId: "4",
+                time: "12:04",
+                author: "info",
+                respId: "",
+                resp: "",
+                msg: "here is a new start"
+            },
+            {
+                msgId: "5",
+                time: "12:05",
+                author: "me",
+                respId: "",
+                resp: "",
+                msg: "message 5"
+            },
+            {
+                msgId: "6",
+                time: "12:06",
+                author: "Flávia",
+                respId: "",
+                resp: "",
+                msg: "answer 6"
+            },
         ]
     },
     {
