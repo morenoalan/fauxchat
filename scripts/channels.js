ad27f878c494e23169c8655d3e41df0ad388ffd5
Alan Moreno <alan.moreno@hotmail.com>
2024-11-14 23:55:27 -0300

    update updates, status, channel

---
+++ scripts/channels.js
@@ -0,0 +1,15 @@
+let channels = [
+    {
+        id: "0",
+        pin: '0',
+        msgs: [
+            {
+                id: "0",
+                time: "2024-09-19 12:00:00 UTC-03:00",
+                author: "00_info",
+                msg: "First messagem from first channel",
+                status: "4"
+            },
+        ]
+    }
+];
